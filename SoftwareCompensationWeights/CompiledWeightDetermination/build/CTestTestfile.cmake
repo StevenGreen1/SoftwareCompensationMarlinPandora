2fcdf73464e23d37f6495de73f4b48e3ec4d9fb4
Steven Green <sg1sg2sg3@hotmail.co.uk>
2016-04-13 16:58:24 +0100

    Removed unnecessary bin and build folders.

--- SoftwareCompensationWeights/CompiledWeightDetermination/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usera/sg568/ilcsoft_v01_17_09/geant4Analysis/Analysis/MultiPlot
-# Build directory: /usera/sg568/ilcsoft_v01_17_09/geant4Analysis/Analysis/MultiPlot/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
